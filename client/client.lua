9d87cfd402b20454d5309d194715ce0d4305fe34
Zereth616 <zeroxzero1@aim.com>
2025-08-09 04:53:17 -0400

    Added in Train hashes and new train interiors

--- client/client.lua
+++ client/client.lua
@@ -1,8 +1,19 @@
+local modelHashMap = {
+    ['appleseed_config'] = 987516329,
+    ['bountyhunter_config'] = 0x515E31ED,
+    ['engine_config'] = 0x487B2BE7,
+    ['gunslinger3_config'] = 0x8EAC625C,
+    ['gunslinger4_config'] = 0xCD2C7CA1 ,
+    ['prisoner_escort_config'] = 0x005E03AD,
+    ['winter4_config'] = 0x3260CE89,
+    ['prisoner_escort_config1'] = 0x005E03AD,
+}
+
 -- Start Train
 CreateThread(function()
     StartMainPrompts()
     SetRandomTrains(false)
-    TriggerServerEvent('bcc-train:BridgeFallHandler', true)
+  --  TriggerServerEvent('bcc-train:BridgeFallHandler', true)
     while true do
         local playerPed = PlayerPedId()
         local pCoords = GetEntityCoords(playerPed)
@@ -179,48 +190,132 @@ CreateThread(function()
     end
 end)
 
-function SpawnTrain(trainCfg, myTrainData, dirChange, station) --credit to rsg_trains for some of the logic here
-    local model = trainCfg.model
-    local trainHash = joaat(model)
+-- ReplaceCabinInterior remains the same but accepts any carriage entity
+local function ReplaceCabinInterior(carriage, interior)
+    local propset = Citizen.InvokeNative(0xCFC0BD09BB1B73FF, carriage)
+    local propsetHash = Citizen.InvokeNative(0xA6A9712955F53D9C, propset)
+    local request = Citizen.InvokeNative(0xF3DE57A46D5585E9, interior)
+
+    if Citizen.InvokeNative(0xF42DB680A8B2A4D9, propset) then
+        while not Citizen.InvokeNative(0x48A88FC684C55FDC, propsetHash) do
+            Wait(1)
+        end
+        Citizen.InvokeNative(0x3BCF32FF37EA9F1D, carriage)
+    else
+        return false
+    end
+
+    while not Citizen.InvokeNative(0x48A88FC684C55FDC, interior) do
+        Wait(1)
+    end
+
+    local attachedPropset = Citizen.InvokeNative(0x9609DBDDE18FAD8C, interior, 0, 0, 0, carriage, 0, true, 0, true)
+
+    Citizen.InvokeNative(0xB1964A83B345B4AB, interior)
+    return true
+end
+
+local function UnlockCabinDoors(carriage)
+    Citizen.InvokeNative(0x550CE392A4672412, carriage, 9, true, true)   -- Open cabin doors
+    Citizen.InvokeNative(0x550CE392A4672412, carriage, 10, true, true)
+    Citizen.InvokeNative(0x550CE392A4672412, carriage, 11, true, true)
+end
+
+-- Main handler accepting a list of carriages
+local function HandleTrainCarriages(train, carriageIndices)
+    local barCreated = false
+
+    CreateThread(function()
+        while true do
+            Wait(1000)
+
+            for _, index in ipairs(carriageIndices) do
+                local carriage = Citizen.InvokeNative(0xD0FB093A4CDB932C, train, index)
+
+                if carriage and #(GetEntityCoords(PlayerPedId()) - GetEntityCoords(carriage)) < 150 then
+                    if index == carriageIndices[2] then
+                        -- First carriage (e.g., bar)
+                        if not barCreated then
+                            barCreated = ReplaceCabinInterior(carriage, -1747631964)
+                        end
+                    elseif index == carriageIndices[3] or index == carriageIndices[4] then
+                        -- Carriages for doors
+                        UnlockCabinDoors(carriage)
+                    end
+                end
+            end
+        end
+    end)
+end
+
+function SpawnTrain(trainCfg, myTrainData, dirChange, station)
+    -- Map the model string to the hash
+    local modelName = trainCfg.model -- e.g., 'appleseed_config'
+    local trainHash = modelHashMap[modelName]
+    if not trainHash then
+        print("Error: No hash found for model:", modelName)
+        return
+    end
+
+    -- Assign train id and fuel/condition data
     TrainFuel = myTrainData.fuel
     TrainCondition = myTrainData.condition
     TrainId = myTrainData.trainid
 
+    -- Load train cars and spawn
     LoadTrainCars(trainHash)
-    MyTrain = Citizen.InvokeNative(0xc239dbd9a57d2a71, trainHash, Stations[station].train.coords, dirChange, false, true, false) -- CreateMissionTrain
-    SetModelAsNoLongerNeeded(model)
-    -- Freeze Train on Spawn
-    Citizen.InvokeNative(0xDFBA6BBFF7CCAFBB, MyTrain, 0.0) -- SetTrainSpeed
-    Citizen.InvokeNative(0x01021EB2E96B793C, MyTrain, 0.0) -- SetTrainCruiseSpeed
+    local coords = Stations[station] and Stations[station].train.coords
+    if not coords then
+        print("Invalid station index or missing coords")
+        return
+    end
 
+    MyTrain = Citizen.InvokeNative(0xc239dbd9a57d2a71, trainHash, coords, dirChange, false, true, false)
+    SetModelAsNoLongerNeeded(trainCfg.model)
+
+    -- Freeze the train at spawn speed
+    Citizen.InvokeNative(0xDFBA6BBFF7CCAFBB, MyTrain, 0.0)
+    Citizen.InvokeNative(0x01021EB2E96B793C, MyTrain, 0.0)
+    
+    -- Server sync
     TriggerServerEvent('bcc-train:UpdateTrainSpawnVar', true, MyTrain)
 
-    if trainCfg.blip.show then
-        local trainBlip = Citizen.InvokeNative(0x23f74c2fda6e7c61, -1749618580, MyTrain) -- BlipAddForEntity
+    -- Blip setup
+    if trainCfg.blip and trainCfg.blip.show then
+        local trainBlip = Citizen.InvokeNative(0x23f74c2fda6e7c61, -1749618580, MyTrain)
         SetBlipSprite(trainBlip, joaat(trainCfg.blip.sprite), true)
-        Citizen.InvokeNative(0x9CB1A1623062F402, trainBlip, trainCfg.blip.name) -- SetBlipNameFromPlayerString
-        Citizen.InvokeNative(0x662D364ABF16DE2F, trainBlip, joaat(Config.blipColors[trainCfg.blip.color])) -- BlipAddModifier
+        Citizen.InvokeNative(0x9CB1A1623062F402, trainBlip, trainCfg.blip.name)
+        Citizen.InvokeNative(0x662D364ABF16DE2F, trainBlip, joaat(Config.blipColors[trainCfg.blip.color] or 'WHITE'))
     end
 
-    if trainCfg.inventory.enabled then
-        TriggerServerEvent('bcc-train:RegisterInventory', TrainId, model)
+    -- Inventory registration
+    if trainCfg.inventory and trainCfg.inventory.enabled then
+        TriggerServerEvent('bcc-train:RegisterInventory', TrainId, trainCfg.model)
     end
 
-    if trainCfg.fuel.enabled then
+    -- Fuel handling
+    if trainCfg.fuel and trainCfg.fuel.enabled then
         TriggerEvent('bcc-train:FuelDecreaseHandler', trainCfg, myTrainData)
     end
 
-    if trainCfg.condition.enabled then
+    -- Condition handling
+    if trainCfg.condition and trainCfg.condition.enabled then
         TriggerEvent('bcc-train:CondDecreaseHandler', trainCfg, myTrainData)
     end
 
-    if trainCfg.fuel.enabled or trainCfg.condition.enabled then
+    -- Show target menu if fuel or condition active
+    if (trainCfg.fuel and trainCfg.fuel.enabled) or (trainCfg.condition and trainCfg.condition.enabled) then
         TriggerEvent('bcc-train:TargetMenu', trainCfg)
     end
+
+    -- Handle train with dynamic carriages
+    local carriageIndicesToHandle = {3, 4, 5, 6} -- customize per train
+    HandleTrainCarriages(MyTrain, carriageIndicesToHandle)
+
+    -- General train handling
     TriggerEvent('bcc-train:TrainHandler', trainCfg, myTrainData)
     TriggerEvent('bcc-train:TrainActions')
 end
-
 AddEventHandler('bcc-train:TrainHandler', function(trainCfg, myTrainData)
     DrivingMenuOpened = false
     while MyTrain do
@@ -263,7 +358,7 @@ AddEventHandler('bcc-train:FuelDecreaseHandler', function(trainCfg, myTrainData)
     while MyTrain do
         Wait(1000)
         if EngineStarted and TrainFuel >= 1 then
-            Wait(trainCfg.fuel.decreaseTime * 1000)
+            Wait(trainCfg.fuel.decreaseTime * 10000)
             local fuel = VORPcore.Callback.TriggerAwait('bcc-train:DecTrainFuel', TrainId, TrainFuel, trainCfg)
             if fuel then
                 FuelUpdate(fuel)
@@ -289,7 +384,7 @@ AddEventHandler('bcc-train:CondDecreaseHandler', function(trainCfg, myTrainData)
     while MyTrain do
         Wait(1000)
         if EngineStarted and TrainCondition >= 1 then
-            Wait(trainCfg.condition.decreaseTime * 1000)
+            Wait(trainCfg.condition.decreaseTime * 100000)
             local cond = VORPcore.Callback.TriggerAwait('bcc-train:DecTrainCond', TrainId, TrainCondition, trainCfg)
             if cond then
                 ConditionUpdate(cond)
