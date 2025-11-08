7917880b0ebd50b8ae767942016a60b955cd28eb
Zereth616 <zeroxzero1@aim.com>
2025-11-08 00:56:29 -0500

    Refactor train management and event handling

--- server/server.lua
+++ server/server.lua
@@ -299,17 +299,3 @@ VORPcore.Callback.Register('bcc-train:CheckPlayerCooldown', function(source, cb,
         cb(false)
     end
 end)
-
--- Check if properly downloaded
-function file_exists(name)
-  local f = LoadResourceFile(GetCurrentResourceName(), name)
-  return f ~= nil
-end
-
-if not file_exists('./ui/index.html') then
-  print('^1 INCORRECT DOWNLOAD!  ^0')
-  print(
-    '^4 Please Download: ^2(bcc-train.zip) ^4from ^3<https://github.com/BryceCanyonCounty/bcc-train/releases/latest>^0')
-end
-
-BccUtils.Versioner.checkRelease(GetCurrentResourceName(), 'https://github.com/BryceCanyonCounty/bcc-train')
